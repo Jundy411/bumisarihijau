6647a3214df97e791ffdd1e0a80fb665e941a82d
Jundy Islami <jundyislami3@gmail.com>
2026-01-19 10:08:37 +0700

    memperbaiki error Navbar, Home, dan App

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Navbar from "./Navbar/Navbar";
 import ScrollTop from "./content/ScrollTop";
 import Home from "./page/Home";
-<<<<<<< HEAD
+
 // import AboutUs from "./page/AboutUs";
-=======
+
 import AboutUs from "./page/AboutUs";
->>>>>>> b671a8f757c4163f69561e8900edb3d0bb1f9d21
+
 import Service from "./page/Service";
 import PortfolioPage from "./page/PortfolioPage";
 import BangunanBaru from "./sections/Service/BangunanBaru";
@@ -17,10 +17,9 @@ import Sipil from "./sections/Service/Sipil";
 import Interior from "./sections/Service/Interior";
 import Konsultasi from "./sections/Service/Konsultasi";
 import ProjectRumah from "./sections/Portfolio/ProjectRumah";
-<<<<<<< HEAD
+
 import Gallery from "./page/Gallery";
-=======
->>>>>>> b671a8f757c4163f69561e8900edb3d0bb1f9d21
+
 const routeList = createBrowserRouter([
   {
     path: "/",
@@ -35,48 +34,41 @@ const routeList = createBrowserRouter([
         element: <Service />,
       },
       {
-        path:"/bangunan-baru",
-        element:<BangunanBaru/>
+        path: "/bangunan-baru",
+        element: <BangunanBaru />,
       },
       {
-        path:"/renovasi",
-        element:<Renovasi/>
+        path: "/renovasi",
+        element: <Renovasi />,
       },
       {
-        path:"/arsitek",
-        element:<Arsitek/>
+        path: "/arsitek",
+        element: <Arsitek />,
       },
       {
-        path:"/sipil",
-        element:<Sipil/>
+        path: "/sipil",
+        element: <Sipil />,
       },
       {
-        path:"/interior",
-        element:<Interior/>
+        path: "/interior",
+        element: <Interior />,
       },
       {
-        path:"/konsultasi",
-        element:<Konsultasi/>
+        path: "/konsultasi",
+        element: <Konsultasi />,
       },
       {
         path: "portfolio",
         element: <PortfolioPage />,
       },
       {
-<<<<<<< HEAD
-        path:"/rumah",
-=======
-        path:"rumah",
->>>>>>> b671a8f757c4163f69561e8900edb3d0bb1f9d21
-        element:<ProjectRumah/>
+        path: "/rumah",
+
+        element: <ProjectRumah />,
       },
       {
         path: "galeri",
-<<<<<<< HEAD
         element: <Gallery />,
-=======
-        element: <AboutUs />,
->>>>>>> b671a8f757c4163f69561e8900edb3d0bb1f9d21
       },
     ],
   },
