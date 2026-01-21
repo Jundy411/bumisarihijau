ea3310f4802157551d2e3ccd5347ab024f82df10
Jundy Islami <jundyislami3@gmail.com>
2026-01-21 10:41:36 +0700

    menambah button sosmed dan responsive tampilan

--- src/sections/Home/Address.jsx
+++ src/sections/Home/Address.jsx
@@ -1,12 +1,12 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import Aos from "aos";
 import "aos/dist/aos.css";
+import { FaMapMarkerAlt, FaFacebookSquare, FaTiktok } from "react-icons/fa";
 
 const Address = () => {
-
   useEffect(() => {
     Aos.init();
-  },[])
+  }, []);
 
   const urlMap =
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d982.9629145816643!2d109.02071148466682!3d-6.849019536625204!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6fb03eff414865%3A0x1e022dbfad8faf39!2sINDOMARET%20SAWOJAJAR!5e1!3m2!1sen!2sid!4v1768445302941!5m2!1sen!2sid";
@@ -22,7 +22,7 @@ const Address = () => {
               <h2 className="font-heading text-3xl md:text-4xl font-bold text-dark mb-10">
                 CV Bumi Sari Hijau
               </h2>
-              <div className=" flex justify-between items-center gap-10">
+              <div className=" flex flex-col lg:flex-row justify-between items-center gap-10">
                 <iframe
                   title="lokasi"
                   src={`${urlMap}`}
@@ -36,12 +36,20 @@ const Address = () => {
                     Jalan jalan dikota, senang bertemu anda, Rt 02 Rw 04 UH 12,
                     Kota Semarang, Jawa Tengah
                   </p>
-                  <button
-                    onClick={() => window.open(urlMap)}
-                    className="px-5 py-2 bg-primary text-white rounded hover:bg-blue-800 transition shadow-lg transform hover:-translate-y-0.5 cursor-pointer"
-                  >
-                    Buka Maps
-                  </button>
+                  <div className=" flex justify-center gap-5">
+                    <button
+                      onClick={() => window.open(urlMap)}
+                      className="px-5 py-2 bg-[#EA4335] flex items-center gap-1 text-white rounded transition shadow-lg cursor-pointer"
+                    >
+                      <span><FaMapMarkerAlt/></span>Maps
+                    </button>
+                    <button className="px-5 py-2 flex items-center gap-1 bg-blue-800 text-white rounded shadow-lg cursor-pointer">
+                      <span><FaFacebookSquare/></span>Facebook
+                    </button>
+                    <button className="px-5 py-2 flex items-center gap-1 bg-black text-white rounded  shadow-lg  cursor-pointer">
+                      <span><FaTiktok/></span>Tik tok
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
